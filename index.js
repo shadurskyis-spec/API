d1d78b105d7a08742f573b9cde6a27d4377e47f0
shadurskyis-spec <shadurskyis@gmail.com>
2026-05-10 17:24:33 +0200

    APIshka commited

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let http = require("http");
+const http = require("http");
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req,res)=>{
-    getAllJokes(req,res);
-})
-server.listen(5000);
+const directoryPath = './data';
+
+const server = http.createServer((req, res) => {
+    
+    const resultArray = [];
+
+
+    const files = fs.readdirSync(directoryPath);
+
+
+    for (let i = 0; i < files.length; i++) {
+        const filePath = path.join(directoryPath, files[i]);
+        const fileContent = fs.readFileSync(filePath);
+
+        
+        const buffer = Buffer.from(fileContent);
+        const obj = JSON.parse(buffer.toString());
+
+        resultArray.push(obj);
+    }
+
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(resultArray)); 
+});
+
+server.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
